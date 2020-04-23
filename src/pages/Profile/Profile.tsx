2224886f2c6158b898eea3136414d23f54af5db7
Zabirov Artem <qwertysairex@gmail.com>
2020-04-23 22:49:38 +0300

    feat(Added RoleUser components)

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { IUserInfo, UserInfo } from './RoleUser/UserInfo';
+import UserStatistic from './RoleUser/UserStatistic';
 
 const Profile: React.FC = () => {
   const userInfo: IUserInfo = {
@@ -21,46 +22,7 @@ const Profile: React.FC = () => {
     <section className="section section_withHeader section-profile">
       <div className="profile-info">
         <UserInfo userData={userInfo} />
-        <div className="profile-work">
-          <h3 className="profile-work__title">Информация о работе</h3>
-          <div className="profile-work-info">
-            <ul className="profile-work-links">
-              <li className="profile-work-links__item">
-                <span className="profile-work-links__text">Как правильно заводить товар:</span>
-                <a
-                  className="profile-work-links__href"
-                  href="https://docs.google.com/document/d/1xLnNghW7Z9TX_Gcsc6yJ19j40-YqV3hSp7EGECWAcu0/edit">
-                  Инструкция
-                </a>
-              </li>
-              <li className="profile-work-links__item">
-                <span className="profile-work-links__text">Алгоритм работы и обязанности:</span>
-                <a
-                  className="profile-work-links__href"
-                  href="https://docs.google.com/document/d/1-0Vwkvh9KWjkUtYQpcsNVS2pSu_jVlJNmC3TeHYRA98/edit">
-                  Инструкция
-                </a>
-              </li>
-              <li className="profile-work-links__item">
-                <span className="profile-work-links__text">Правила ведения отчетости:</span>
-                <a
-                  className="profile-work-links__href"
-                  href="https://docs.google.com/document/d/1fQOQrmhlnqgYmKzmNsCIclMlL4VbxhR8N08bbGFWwRE/edit">
-                  Инструкция
-                </a>
-              </li>
-              <li className="profile-work-links__item">
-                <span className="profile-work-links__text">Список партнеров:</span>
-                <a
-                  className="profile-work-links__href"
-                  href="https://docs.google.com/document/d/1fQOQrmhlnqgYmKzmNsCIclMlL4VbxhR8N08bbGFWwRE/edit">
-                  Список
-                </a>
-              </li>
-            </ul>
-            <div className="profile-work-offical"></div>
-          </div>
-        </div>
+        <UserStatistic />
       </div>
     </section>
   );
